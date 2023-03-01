5fbbf6dc77ab2865706bde8c6c86712a73152030
qa-esire-3-dtmd@example.com <qa-esire-3-dtmd@example.com>
2023-03-01 18:12:56 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15263.stoplight-dev.com/api/v1/projects/cHJqOjE4NTU/spectral.js?branch_slug=main&token=ef6d2735-c30a-4564-879c-e067ab039cd1";
+export default { extends: ruleset };
